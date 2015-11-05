a86bd08d845a3d5d6bf28a6f0746313548d1b133
guilledifabio <guillermodifabio@gmail.com>
2015-11-05 17:31:24 -0300

    Tercer Commit

--- src/api/Reclamos.java
+++ src/api/Reclamos.java
@@ -9,19 +9,19 @@ import modelo.Ciudadano;
 
 public class Reclamos {
 	
-	private String nombreDB = "reclamos";
-	private static ObjectContainer db;
 	
-	public void conectarDB() {
-		db = Db4oEmbedded.openFile(Db4oEmbedded.newConfiguration(), nombreDB);
-	}
-	
-	public void desconectarDB() {
-		db.close();
-	}
-	
-	public Ciudadano buscarCiudadano(int DNI) {
 
+		private String nombreDB = "reclamos";
+		private static ObjectContainer db;
+			
+			public void conectarDB() {
+				db = Db4oEmbedded.openFile(Db4oEmbedded.newConfiguration(), nombreDB);
+			}
+			
+			public void desconectarDB() {
+				db.close();
+			}
+			public Ciudadano buscarCiudadano(int DNI) {
 		CiudadanoDaoImpl ciudadanoDAO = new CiudadanoDaoImpl();
 		ObjectSet<Ciudadano> ciudadanos = ciudadanoDAO.buscarTodos(db);
 		System.out.println("buscar ciudadano "+ciudadanos.size());
@@ -30,7 +30,8 @@ public class Reclamos {
 				return ciudadano;
 			}
 		}
-		
+	
 		return null;
 	}
+
 }
